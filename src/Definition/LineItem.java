0ca96307fe71bba182a92785cb233d806897ed6a
Kshitij Sahu <kshitij.sahu_cs19@gla.ac.in>
2021-04-19 00:12:15 +0530

    create methode to getTotalPrice

--- src/Definition/LineItem.java
+++ src/Definition/LineItem.java
@@ -37,6 +37,7 @@ public class LineItem {
 
 
         return quantity * price;
+
     }
 
     @Override
--- src/Execution/Transaction.java
+++ src/Execution/Transaction.java
@@ -1,4 +1,58 @@
 package Execution;
 
+
+import Definition.LineItem;
+
+import javax.xml.namespace.QName;
+import java.util.ArrayList;
+
 public class Transaction {
+
+    public final ArrayList<LineItem> lineItems;
+    private int customersID;
+    private String customersName;
+    private String LineItem;
+    private int i;
+
+    public Transaction(int customersID, String customersName) {
+        this.customersID = customersID;
+        this.customersName = customersName;
+        lineItems = new ArrayList<>();
+    }
+
+    public int getCustomersID() {
+        return customersID;
+    }
+
+    public void setCustomersID(int customersID) {
+        this.customersID = customersID;
+    }
+
+    public String getCustomersName() {
+        return customersName;
+    }
+
+    public void setCustomersName(String customersName) {
+        this.customersName = customersName;
+    }
+
+    public Definition.LineItem addLineItem(String itemName, int Quantity, double price){
+        lineItems.add(addLineItem(itemName,Quantity,price));
+        return null;
+    }
+     public void UpdateItem(String itemname,int Quantity,double price){
+        for (LineItem lineItem : lineItems){
+            if (lineItem.getItemName().equalsIgnoreCase()){
+                lineItem.setPrice(price);
+                lineItem.setQuantity(Quantity);
+                break;
+            }
+        }
+     }
+     public double getTotalPrice(){
+        return
+     }
+
 }
+
+
